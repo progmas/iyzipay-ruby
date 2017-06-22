ef912a00d0ffb7ae6b2ded7580069bde2faf63c1
Yalçın Yenigün <yalcin.yenigun@iyzico.com>
2017-06-22 10:45:02 +0300

    rspec expectations added for functional tests on sandbox environment

--- spec/api_test_spec.rb
+++ spec/api_test_spec.rb
@@ -5,8 +5,8 @@ require_relative 'spec_helper'
 RSpec.describe 'Iyzipay' do
   before :all do
     @options = Iyzipay::Options.new
-    @options.api_key = 'your api key'
-    @options.secret_key = 'your secret key'
+    @options.api_key = 'sandbox-wWhcq20IuWvDsLNjfbTLq84GXUKrjt40'
+    @options.secret_key = 'sandbox-y8oXzpVZso1Nr5n6ER3Tegt9rWNRqytV'
     @options.base_url = 'https://sandbox-api.iyzipay.com'
   end
 
@@ -14,6 +14,10 @@ RSpec.describe 'Iyzipay' do
     api_test = Iyzipay::Model::ApiTest.new.retrieve(@options)
     begin
       $stderr.puts api_test.inspect
+      api_test =  JSON.parse(api_test)
+      expect(api_test['status']).to eq('success')
+      expect(api_test['locale']).to eq('tr')
+      expect(api_test['systemTime']).not_to be_nil
     rescue
       $stderr.puts 'oops'
       raise
