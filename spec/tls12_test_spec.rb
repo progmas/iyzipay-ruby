43e46616de27d17e216e1374c794218791df771e
Mert Akengin <mert.akengin@iyzico.com>
2019-10-16 10:42:54 +0300

    undo rvm 2.1 and fix tls1.2 url

--- spec/tls12_test_spec.rb
+++ spec/tls12_test_spec.rb
@@ -5,7 +5,7 @@ require_relative 'spec_helper'
 RSpec.describe 'Iyzipay' do
   before :all do
     @options = Iyzipay::Options.new
-    @options.base_url = 'https://sandbox-api-tls12.iyzipay.com/'
+    @options.base_url = 'https://sandbox-api.iyzipay.com/'
   end
 
   it 'should test tls 1.2 support' do
